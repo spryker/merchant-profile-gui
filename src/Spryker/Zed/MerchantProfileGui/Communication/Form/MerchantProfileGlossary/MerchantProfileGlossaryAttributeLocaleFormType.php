9aec043f9e952b0b6944d230d7e6bbdd1383a5ea
artemkosatov <artem.kosatov@spryker.com>
2019-10-18 16:21:07 +0300

    MP-1179: BE refactoring #1 after AA review.

--- src/Spryker/Zed/MerchantProfileGui/Communication/Form/MerchantProfileGlossary/MerchantProfileGlossaryAttributeLocaleFormType.php
+++ src/Spryker/Zed/MerchantProfileGui/Communication/Form/MerchantProfileGlossary/MerchantProfileGlossaryAttributeLocaleFormType.php
@@ -8,16 +8,18 @@
 namespace Spryker\Zed\MerchantProfileGui\Communication\Form\MerchantProfileGlossary;
 
 use Spryker\Zed\Kernel\Communication\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 /**
  * @method \Spryker\Zed\MerchantProfileGui\MerchantProfileGuiConfig getConfig()
  * @method \Spryker\Zed\MerchantProfileGui\Communication\MerchantProfileGuiCommunicationFactory getFactory()
  */
-class MerchantProfileLocalizedGlossaryAttributesFormType extends AbstractType
+class MerchantProfileGlossaryAttributeLocaleFormType extends AbstractType
 {
-    protected const FIELD_MERCHANT_PROFILE_GLOSSARY_ATTRIBUTES_LOCALE = 'locale';
-    protected const FIELD_MERCHANT_PROFILE_GLOSSARY_ATTRIBUTES = 'merchantProfileGlossaryAttributeValues';
+    protected const FIELD_ID_LOCALE = 'idLocale';
+    protected const FIELD_LOCALE_NAME = 'localeName';
 
     /**
      * @param \Symfony\Component\Form\FormBuilderInterface $builder
@@ -27,8 +29,8 @@ class MerchantProfileLocalizedGlossaryAttributesFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $this->addMerchantProfileGlossaryAttributeLocaleField($builder)
-            ->addMerchantProfileGlossaryAttributeValuesSubform($builder);
+        $this->addIdLocaleField($builder)
+            ->addLocaleNameField($builder);
     }
 
     /**
@@ -36,11 +38,13 @@ class MerchantProfileLocalizedGlossaryAttributesFormType extends AbstractType
      *
      * @return $this
      */
-    protected function addMerchantProfileGlossaryAttributeLocaleField(FormBuilderInterface $builder)
+    protected function addIdLocaleField(FormBuilderInterface $builder)
     {
-        $builder->add(static::FIELD_MERCHANT_PROFILE_GLOSSARY_ATTRIBUTES_LOCALE, MerchantProfileGlossaryAttributeLocaleFormType::class, [
+        $builder->add(static::FIELD_ID_LOCALE, HiddenType::class, [
             'label' => false,
         ]);
+
+        $builder->get(static::FIELD_ID_LOCALE)->addModelTransformer($this->createStringToNumberTransformer());
 
         return $this;
     }
@@ -50,12 +54,27 @@ class MerchantProfileLocalizedGlossaryAttributesFormType extends AbstractType
      *
      * @return $this
      */
-    protected function addMerchantProfileGlossaryAttributeValuesSubform(FormBuilderInterface $builder)
+    protected function addLocaleNameField(FormBuilderInterface $builder)
     {
-        $builder->add(static::FIELD_MERCHANT_PROFILE_GLOSSARY_ATTRIBUTES, MerchantProfileGlossaryAttributeValuesFormType::class, [
+        $builder->add(static::FIELD_LOCALE_NAME, HiddenType::class, [
             'label' => false,
         ]);
 
         return $this;
+    }
+
+    /**
+     * @return \Symfony\Component\Form\CallbackTransformer
+     */
+    protected function createStringToNumberTransformer(): CallbackTransformer
+    {
+        return new CallbackTransformer(
+            function ($value) {
+                return (string)$value;
+            },
+            function ($value) {
+                return (int)$value;
+            }
+        );
     }
 }
